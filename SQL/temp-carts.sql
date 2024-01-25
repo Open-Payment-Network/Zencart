5f28afe08d62dd8b114b6a6f0d51fb1bdb9da096
laurence bartrop-clist <laurence.bartrop-clist@cardstream.com>
2024-01-25 14:38:38 +0000

    CS-2400: Fixed formatting on SQL files

--- SQL/temp-carts.sql
+++ SQL/temp-carts.sql
@@ -1,1 +1,5 @@
-CREATE TABLE IF NOT EXISTS paymentnetwork_temp_carts (paymentnetwork_orderRef VARCHAR(64) NOT NULL, paymentnetwork_session TEXT NOT NULL, paymentnetwork_cdate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, paymentnetwork_orderID int NULL) ENGINE=InnoDB;
+CREATE TABLE IF NOT EXISTS paymentnetwork_temp_carts (
+paymentnetwork_orderRef VARCHAR(64) NOT NULL, paymentnetwork_session TEXT NOT NULL,
+paymentnetwork_cdate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+paymentnetwork_orderID int NULL
+) ENGINE=InnoDB;
