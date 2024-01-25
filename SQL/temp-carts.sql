a14626495b04a20ca0493cdf567900c9afb18269
laurence bartrop-clist <laurence.bartrop-clist@cardstream.com>
2024-01-25 14:47:22 +0000

    Updated formatting on SQL file

--- SQL/temp-carts.sql
+++ SQL/temp-carts.sql
@@ -2,5 +2,4 @@ CREATE TABLE IF NOT EXISTS paymentnetwork_temp_carts (
 	paymentnetwork_orderRef VARCHAR(64) NOT NULL, paymentnetwork_session TEXT NOT NULL,
 	paymentnetwork_cdate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	paymentnetwork_orderID int NULL
-) 
-ENGINE=InnoDB;
+) ENGINE=InnoDB;
